509fb65dc2b4bf538501e4753f4f95049734eabb
Atikocak <atilimkocak@gmail.com>
2025-01-11 17:06:15 +0300

    Project completed

--- Sprint-14/Lesson-2/Project-1/src/main/java/org/example/Main.java
+++ Sprint-14/Lesson-2/Project-1/src/main/java/org/example/Main.java
@@ -1,7 +1,33 @@
 package org.example;
 
+import org.example.model.*;
+import org.example.model.enums.LampType;
+import org.example.model.enums.PaintColor;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        Wall wall1 = new Wall("North");
+        wall1.create();
+        Wall wall2 = new Wall("East");
+        wall2.create();
+        Wall wall3 = new Wall("South");
+        wall3.create();
+        Wall wall4 = new Wall("West");
+        wall4.create();
+        Ceiling ceiling = new Ceiling(130, PaintColor.RED);
+        ceiling.create();
+        Bed bed = new Bed("Two Person", 2, 200, 180, 2);
+        bed.make();
+        Lamp lamp = new Lamp(LampType.LAVA, true, 10);
+        lamp.turnOn();
+        Wardrobe wardrobe = new Wardrobe(150, 220, 18);
+        wardrobe.add();
+        Carpet carpet = new Carpet(100, 200, PaintColor.RED);
+        carpet.lying();
+
+        Bedroom bedroom = new Bedroom("Adult Bedroom" ,wall1, wall2, wall3, wall4, ceiling, bed, lamp, wardrobe, carpet);
+        System.out.println(bedroom);
     }
 }
--- Sprint-14/Lesson-2/Project-1/src/main/java/org/example/model/enums/PaintColor.java
+++ Sprint-14/Lesson-2/Project-1/src/main/java/org/example/model/enums/PaintColor.java
@@ -1,4 +1,7 @@
 package org.example.model.enums;
 
 public enum PaintColor {
+    RED,
+    GREEN,
+    WHITE;
 }
